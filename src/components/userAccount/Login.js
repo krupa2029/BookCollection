db99c1d8257a20408a8a8f9a7f510f36b835f114
krupa2029 <KrupaK@indusa.com>
2022-04-18 19:04:12 +0530

    Add: validation in Login form Use: Formik and Yup

--- src/components/userAccount/Login.js
+++ src/components/userAccount/Login.js
@@ -1,23 +1,72 @@
+import { Formik, useFormik } from "formik";
 import React from "react";
 import classes from "./Login.module.css";
+import * as Yup from "yup";
 
 export const Login = () => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid Email Format!").required("Required!"),
+    password: Yup.string()
+      .required("Please enter your password")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+        "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+      ),
+  });
+
+  const onSubmit = (values) => {
+    console.log("Form Data", values);
+  };
+
   return (
-    <section className={classes.auth}>
-      <h3>Login</h3>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" required />
-        </div>
-        <div className={classes.actions}>
-          <button>Login</button>
-        </div>
-      </form>
-    </section>
+    <Formik
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+      initialValues={initialValues}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        touched,
+        isValid,
+        errors,
+      }) => (
+        <section className={classes.auth}>
+          <h3>Login</h3>
+          <form onSubmit={handleSubmit}>
+            <div className={classes.control}>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                onChange={handleChange}
+                value={values.email}
+              />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                onChange={handleChange}
+                value={values.password}
+              />
+            </div>
+            <div className={classes.actions}>
+              <button type="submit" disabled={!isValid}>Login</button>
+            </div>
+          </form>
+        </section>
+      )}
+    </Formik>
   );
 };
